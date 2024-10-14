ef16ef7bf68b583ac1c70e7d156626b6ab3b22ab
Santiago Lopez <slopezr10@miumg.edu.gt>
2024-10-13 22:03:40 -0600

    First commit

---
+++ init.sql
@@ -0,0 +1,3 @@
+CREATE USER 'exporter'@'localhost' IDENTIFIED BY 'exporteradmin123' WITH MAX_USER_CONNECTIONS 3;
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'localhost';
+FLUSH PRIVILES;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Grafana, Prometheus, Mysql y Postgres
+
+Proyecto para monitorización de base de datos de `mysql` y `postgres` con Prometheus y Grafana, todo corriendo con docker
